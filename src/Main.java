d6bef18258d6ed05357c8b5762fb461aa325b8fc
Claudio Vittorio Carta <claudio.vittorio.carta@gmail.com>
2023-07-20 00:38:51 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String stringa1 = "Hello";
+        String stringa2 = "How are you?";
+
+        char[] charArray = { stringa1.charAt(0), stringa1.charAt(1), stringa2.charAt(10), stringa2.charAt(11)};
+        System.out.println(charArray);
+    }
+}
